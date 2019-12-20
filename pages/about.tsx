6d308e94c5a2ccdf866536e678b64612e965c936
kunsam <kunsam624@icloud.com>
2019-12-20 13:17:45 +0800

    add about

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+class AboutPage extends React.Component<any, any> {
+  render() {
+    return <div style={{ textAlign: "center" }}>AboutPage</div>;
+  }
+}
+
+export default AboutPage;
