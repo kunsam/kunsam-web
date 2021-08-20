13a29204318650b4e6d4822438678e6b10941db2
kunsam <kunsam624@icloud.com>
2021-08-20 23:35:49 +0800

    feat: add new post

---
+++ database/contents/mypost/juntong/index.tsx
@@ -0,0 +1,65 @@
+import styles from "./index.module.scss";
+
+const JUNTONG_POST = () => (
+  <div className={styles.post}>
+    <p className="content">
+      他来了，
+      <br />
+      他走了，
+      <br />
+      夜晚秋风拜访，
+      <br />
+      回忆醒了，
+      <br />
+      和啵啵一起叫唤着你，
+      <br />
+      怀念酣睡了，
+      <br />
+      等着下一次远走他乡。
+    </p>
+
+    <p className="content">
+      命运的选择总在一念之间，
+      <br />
+      那么一念你想去闯闯，
+      <br />
+      要闯去新奇纷繁的世界，
+      <br />
+      那么一念我想来看看，
+      <br />
+      看到了雨天奔跑的蜗牛。
+    </p>
+
+    <p className="content">
+      差点说再见，
+      <br />
+      你笑着对我说来这里，
+      <br />
+      这里有科技，
+      <br />
+      这里有美丽，
+      <br />
+      这里有眼神熠熠的你。
+    </p>
+
+    <p className="content">
+      差点无法遇见，
+      <br />
+      会错过的不是美丽的姑娘，
+      <br />
+      是一颗纯真善良美好的心灵，
+      <br />
+      也许我早就认识了你，
+      <br />
+      那么一眼就是永远。
+    </p>
+
+    <p className="content">
+      你等来了孤独的光辉，
+      <br />
+      你等到了爱你的纯真。
+    </p>
+  </div>
+);
+
+export default JUNTONG_POST;
--- database/mypost_map.ts
+++ database/mypost_map.ts
@@ -1,4 +1,5 @@
 import MYPOST_1LINE from "./contents/mypost/1line/1line";
+import JUNTONG_POST from "./contents/mypost/juntong";
 import post2 from "./contents/mypost/post2";
 import post3 from "./contents/mypost/post3";
 import post4 from "./contents/mypost/post4";
@@ -24,13 +25,32 @@ export interface MYPost {
 
 const POSTS: MYPost[] = [
   {
+    id: "post_1line_new",
+    title: "《给俊彤写首诗》",
+    brief: {
+      backgroundUrl:
+        "/static/image/mypost/laura-pratt--NqtqonICOM-unsplash.jpg",
+    },
+    content: {
+      backgroundUrl: "/static/image/mypost/491629472815_pic.jpg",
+    },
+
+    musicUrl: "/static/music/zongcilang1.mp3",
+
+    desc: "一起度过漫长岁月",
+
+    client: {
+      renderContent: JUNTONG_POST,
+    },
+  },
+  {
     id: "post_1line",
     title: "《一线之隔》",
     brief: {
-      backgroundUrl: "/static/image/mypost/1line_1.png"
+      backgroundUrl: "/static/image/mypost/1line_1.png",
     },
     content: {
-      backgroundUrl: "/static/image/mypost/1line_2.png"
+      backgroundUrl: "/static/image/mypost/1line_2.png",
     },
 
     musicUrl: "/static/music/SunriseAtParadi.mp3",
@@ -38,17 +58,17 @@ const POSTS: MYPost[] = [
     desc: " 人的一生相隔很远\n远到只有一线之隔",
 
     client: {
-      renderContent: MYPOST_1LINE
-    }
+      renderContent: MYPOST_1LINE,
+    },
   },
   {
     id: "post_2",
     title: "《生如雪花》",
     brief: {
-      backgroundUrl: "/static/image/mypost/post2_brief.png"
+      backgroundUrl: "/static/image/mypost/post2_brief.png",
     },
     content: {
-      backgroundUrl: "/static/image/mypost/post2_content.png"
+      backgroundUrl: "/static/image/mypost/post2_content.png",
     },
 
     musicUrl: "/static/music/Snowflake.mp3",
@@ -56,98 +76,98 @@ const POSTS: MYPost[] = [
     desc: " 我们犹如这雪花\n在空中洋洋洒洒",
 
     client: {
-      renderContent: post2
-    }
+      renderContent: post2,
+    },
   },
 
   {
     id: "post_3",
     title: "《停》",
     brief: {
-      backgroundUrl: "/static/image/mypost/post3_brief.png"
+      backgroundUrl: "/static/image/mypost/post3_brief.png",
     },
 
     content: {
-      backgroundUrl: "/static/image/mypost/post3_content.png"
+      backgroundUrl: "/static/image/mypost/post3_content.png",
     },
     musicUrl: "/static/music/蔡健雅 - 停格.mp3",
     desc: "我在走走停停中，过完了一生",
     client: {
-      renderContent: post3
-    }
+      renderContent: post3,
+    },
   },
 
   {
     id: "post_4",
     title: "《梧桐树下》",
     brief: {
-      backgroundUrl: "/static/image/mypost/post4_brief.jpeg"
+      backgroundUrl: "/static/image/mypost/post4_brief.jpeg",
     },
 
     content: {
-      backgroundUrl: "/static/image/mypost/post4_content.png"
+      backgroundUrl: "/static/image/mypost/post4_content.png",
     },
     musicUrl: "/static/music/N.K.杨凯 - 深秋的雨.mp3",
     desc: "无论怎样的结局，把日复一日的美梦写成歌曲",
     client: {
-      renderContent: post4
-    }
+      renderContent: post4,
+    },
   },
 
   {
     id: "post_5",
     title: "《Sword and Shield —— Sacrifice》",
     brief: {
-      backgroundUrl: "/static/image/mypost/post5_brief.jpeg"
+      backgroundUrl: "/static/image/mypost/post5_brief.jpeg",
     },
 
     content: {
-      backgroundUrl: "/static/image/mypost/post5_content.jpeg"
+      backgroundUrl: "/static/image/mypost/post5_content.jpeg",
     },
     musicUrl: "/static/music/Swordland.mp3",
     desc: "Do be afraid and enjoy your favorite life",
     client: {
-      renderContent: post5
-    }
+      renderContent: post5,
+    },
   },
 
   {
     id: "post_6",
     title: "《精神分裂 —— 爱》",
     brief: {
-      backgroundUrl: "/static/image/mypost/post6_brief.jpeg"
+      backgroundUrl: "/static/image/mypost/post6_brief.jpeg",
     },
 
     content: {
-      backgroundUrl: "/static/image/mypost/post6_content.jpeg"
+      backgroundUrl: "/static/image/mypost/post6_content.jpeg",
     },
     musicUrl: "/static/music/洛轻衣,XHIRO - Dancing Work.mp3",
     desc: "爱在精神分裂的夜。",
     client: {
-      renderContent: post6
-    }
+      renderContent: post6,
+    },
   },
 
   {
     id: "post_7",
     title: "《精神分裂 —— 夜》",
     brief: {
-      backgroundUrl: "/static/image/mypost/post7_brief.jpeg"
+      backgroundUrl: "/static/image/mypost/post7_brief.jpeg",
     },
 
     content: {
-      backgroundUrl: "/static/image/mypost/post7_content.jpeg"
+      backgroundUrl: "/static/image/mypost/post7_content.jpeg",
     },
     musicUrl: "/static/music/高梨康治 - 五月雨.mp3",
     desc: "五彩缤纷的夜，亦真亦幻",
     client: {
-      renderContent: post7
-    }
-  }
+      renderContent: post7,
+    },
+  },
 ];
 
 const MY_POSTS_MAP: Map<string, MYPost> = new Map();
-POSTS.forEach(post => {
+POSTS.forEach((post) => {
   MY_POSTS_MAP.set(post.id, post);
 });
 
